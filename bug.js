87547587e0c08572e8b40b93ae90814179e5702b
BugHunter-X <askzenai@gmail.com>
2025-01-06 11:40:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when using the Expo Go app to test an app that uses a library that requires a native module. The error message is not very helpful, and it can be difficult to track down the root cause.  For example, an app using react-native-vision-camera might produce this cryptic error. 
